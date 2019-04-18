33566b63f93078a2c89e9a311c1627cb91c12e75
chengyu <chengyu0316@gmail.com>
2019-04-18 23:29:43 +1000

    init

---
+++ config.fish
@@ -0,0 +1,17 @@
+set -U fish_user_paths ~/.local/bin/ $fish_user_paths
+set -U fish_user_paths ~/.npm/bin/ $fish_user_paths
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/chengyu/google-cloud-sdk/path.fish.inc' ]; . '/home/chengyu/google-cloud-sdk/path.fish.inc'; end
+
+eval (direnv hook fish)
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
+# tabtab source for slss package
+# uninstall by removing these lines or running `tabtab uninstall slss`
+[ -f /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /home/chengyu/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
